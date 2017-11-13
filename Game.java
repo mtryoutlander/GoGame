f57ab679e80ebb0a554b6dfd6b0499e7dce99d0e
akash56 <akashpokhrel56@gmail.com>
2017-11-13 10:13:21 -0600

    1st prototype about Game class(v2)

--- Game.java
+++ Game.java
@@ -3,52 +3,62 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+In this file, Sanjay declare the function and me(akash) defines the function, we did kind of pair programming//
+*/
 package teamgogo;
 
 /**
  *
  * @author akash
  */
-public class Game {
-    
+public class Game //Main class
+{
+    boolean gameEnd=false;
     private int newAttr,passCount,blackPieces,whitePieces;
-    public GameUI gameUI=new GameUI();
-    public Game(){
+GameBoard gmBoard;    
+
+    public Game() //constructor 
+    {
         newAttr=0;
         passCount=0;
         blackPieces=0;
         whitePieces=0;
+        gmBoard=new GameBoard();
+//GameBoard gmBoard=new GameBoard();
     }
     public void score(){
     }
     public void victory(){
     }
     public void endTurn(){
-    }
-    public void pass(int passCount){
-        switch (passCount){
-            case 0:
-                passCount++;
-                endTurn();
-                break;
-            case 1:
-                passCount++;
-                endGame();
-                break;
-            default:
-                gameUI.passCount=0;
-        }
     
-        
-        
     }
-    public void play(){
+    public void pass()//This method helps to update the class variable pass
+    {
+        if (this.passCount==0){
+               
+        passCount++;
+        this.endTurn();
+        }else{
+        
+        endGame();        
+                }
+    }    
+public void play(){
     }
     public void quit(){
+    
     }
-    public void endGame(){
-    }
-    public void resetTimer(){
+    public void endGame()//the GameUI is updated when the endgame happens
+    { 
+        this.gameEnd=true;
     }
     
+    public void resetTimer(){
+    
+    }
+    
+
+
 }
